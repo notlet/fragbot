02b73243475957d2ddebb002fc9d053cdc4ebb81
LetGame <letgamemc@gmail.com>
2022-06-29 14:04:47 +0300

    0.1.0 Init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Fragbot
+This is a simple program to host and automate a fragrun bot for Hypixel Skyblock.
+
+## Features
+- Auto Send to Limbo.
+- Queue system if multiple users party it at a time.
+- Simple configuration.
+
+## Installation
+- Rename `config.example.json` to `config.json` and fill out the fields.
+- Run `npm install`.
+- Done! 
+
+## Usage
+- To start the bot, run `npm run start` or `node main.js`.
+- The bot will join Hypixel and automatically get into Limbo.
+- When it recieves a party invite, it accepts it and then leaves after 5 seconds (configurable in `config.js`, `fragbot.waitTime` field).
---
+++ main.js
@@ -0,0 +1,55 @@
+const mineflayer = require("mineflayer");
+const log = (...args) => require("process").stdout.write(args.join("") + "[0m\n");
+const config = require("./config.json");
+
+const bot = mineflayer.createBot({
+    host: "mc.hypixel.net",
+    port: "25565",
+    username: config.minecraft.username,
+    password: config.minecraft.password,
+    version: "1.8.9",
+    auth: config.minecraft.accountType,
+})
+
+let inLimbo = false;
+
+bot.on("login", () => {
+    if (!inLimbo) {
+        log("Bot joined a lobby.");
+        setTimeout(() => limbo(), 2000);
+    }
+})
+
+let partyQueue = [];
+let currentlyInParty = false;
+let processQueue = () => {
+    if (currentlyInParty) return;
+    currentlyInParty = true;
+    bot.chat(`/party join ${partyQueue[0]}`);
+    log(`[32mJoined ${partyQueue[0]}'s party, waiting 5 seconds...`)
+    setTimeout(() => {
+        bot.chat('/party leave');
+        log(`[31mLeft ${partyQueue.shift()}'s party. There are ${partyQueue.length} more users waiting.`);
+        currentlyInParty = false;
+        if (partyQueue.length > 0) processQueue();
+    }, config.fragbot.waitTime * 1000);
+}
+
+bot.on("message", event => {
+    let message = event.toString().trim();
+    if (message.endsWith(' the lobby!') || message.endsWith(' the lobby! <<<')) limbo();
+    if (message.includes("You were spawned in Limbo.")) log("[32mSuccessfully spawned in Limbo.");
+    if (message.includes("has invited you to join their party!")) {
+        let user = message.match(/^(?:\[.+\]|)\s*(.+) has invited you to join their party!/m)[1];
+        partyQueue.push(user);
+        log(`${user} partied, adding to queue...`);
+        processQueue();
+    }
+    if (config.fragbot.logAllMessages) log(message);
+})
+
+const limbo = () => {
+    log('[33mSending bot to Limbo...');
+    Array.from(Array(12)).forEach(() => bot.chat("/"));
+    inLimbo = true;
+}
